1da5665083fa4773576de1b8d804c4a3907e44b9
devhindo <lildevhind@gmail.com>
2024-04-18 22:15:21 +0200

    feat: tricked render to prevent sleeping

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"net/http"
-
+	"time"
+	"fmt"
 	"github.com/devhindo/katty/katty"
 )
 
@@ -16,6 +17,18 @@ func main() {
 			w.Write([]byte("Hello from katty!"))
 		})
 		http.ListenAndServe(":8080", nil)
+	}()
+
+	go func() {
+		for {
+			_, err := http.Get("https://katty-mqkx.onrender.com/")
+			if err != nil {
+				// Handle error
+				fmt.Println(err)
+			}
+			// Wait for 14 minutes before sending the next request
+			time.Sleep(14 * time.Minute)
+		}
 	}()
 
 	select {}
